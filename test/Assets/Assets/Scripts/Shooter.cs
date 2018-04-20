2fb930508d5abb754701d2fb6ede191fbda4cbc5
assafgames <assaf.ipad42@gmail.com>
2018-04-20 19:52:52 +0300

    init

---
+++ test/Assets/Assets/Scripts/Shooter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+
+    public Rigidbody projectile;
+    public Transform gun;
+    public int bulletSpeed = 20;
+
+    void Update()
+    {
+        // Put this in your update function
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Instantiate the projectile at the position and rotation of this transform
+            Rigidbody clone = Instantiate(projectile, gun.transform.position, gun.transform.rotation);
+
+            clone.velocity = gun.transform.forward * bulletSpeed;
+
+            //Destroy(clone.gameObject, bulletLifeTime);
+        }
+    }
+}
---
+++ test/Assets/Assets/Scripts/bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+
+    public GameObject boomPrefab;
+
+    void OnCollisionEnter(Collision other)
+    {
+        Instantiate(boomPrefab, transform.position, transform.rotation);
+
+		Destroy(this.gameObject);
+    }
+
+}
